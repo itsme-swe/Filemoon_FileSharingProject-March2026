e258662b180f72f989aa11ae7f9412831f05c4ff
itsharsh_swe <harsh_s.dev@outlook.com>
2026-03-16 22:07:26 +0530

    sharefile model creating

---
+++ model/share.model.js
@@ -0,0 +1,21 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const shareSchema = new Schema(
+  {
+    from: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const ShareModel = model("Share", shareSchema);
+
+module.exports = ShareModel;
